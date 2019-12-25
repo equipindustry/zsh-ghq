75bad15b2aec6e4f125009b4ad523ee91bd01472
@slovacus <slovacus@gmail.com>
2019-12-25 13:37:25 -0500

    (#19) chore: implement function for migrate repositories

--- zsh-ghq.zsh
+++ zsh-ghq.zsh
@@ -8,11 +8,85 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
+GHQ_ROOT=$(ghq root)
 GHQ_CACHE_DIR="${HOME}/.cache/ghq"
 GHQ_CACHE_NAME="ghq.txt"
 GHQ_CACHE_PROJECT="${GHQ_CACHE_DIR}/${GHQ_CACHE_NAME}"
 
 ghq_package_name='ghq'
+
+
+function ghq::is_dir {
+    local target_dir
+    target_dir="${1}"
+    if [ ! -d "${target_dir}" ]; then
+        return 0
+    fi
+    return 1
+}
+
+function ghq::git::get_origin_path {
+    local target_dir
+    local origin_path
+    target_dir="${1}"
+    origin_path=$(cd "${target_dir}" || exit; git config --get-regexp remote.origin.url | cut -d ' ' -f 2)
+    echo "${origin_path}"
+}
+
+function ghq::get_remote_path_from_url {
+    # git remote url may be
+    # ssh://git@hoge.host:22/var/git/projects/Project
+    # git@github.com:motemen/ghq.git
+    # (normally considering only github is enough?)
+    # remove ^.*://
+    # => remove ^hoge@ (usually git@ ?)
+    #  => replace : => /
+    #   => remove .git$
+    local remote_path
+    remote_path=$(echo "${1}" | sed -e 's!^.*://!!; s!^.*@!!; s!:!/!; s!\.git$!!;')
+    echo "${remote_path}"
+}
+
+function ghq::migrate::move {
+    local target_dir
+    local remote_path
+    local new_repo_dir
+    target_dir="${1}"
+    remote_path="${2}"
+
+    message_info "move this repository to ${GHQ_ROOT}/${remote_path}"
+
+    new_repo_dir="${GHQ_ROOT}/${remote_path}"
+
+    if [ -e "${new_repo_dir}" ]; then
+        message_error "${new_repo_dir} already exists!!!!"
+    fi
+    mkdir -p "${new_repo_dir%/*}"
+    mv "${target_dir%/}" "${new_repo_dir}"
+    message_success "${new_repo_dir} migrate!!!!"
+}
+
+# migrate repository path to root path
+function ghq::migrate {
+    local target_dir
+    local origin_path
+    local migrate_path
+
+    target_dir=$1
+    if [ ! "$(ghq::is_dir "${target_dir}")" ]; then
+        message_info "${target_dir} not is directory"
+    fi
+
+    origin_path=$(ghq::git::get_origin_path "${target_dir}")
+
+    if [ -z "${origin_path}" ]; then
+        message_info "not found repository remote"
+    fi
+
+    migrate_path="$(ghq::get_remote_path_from_url "${origin_path}")"
+
+    ghq::migrate::move "${target_dir}" "${migrate_path}"
+}
 
 function ghq::dependences::check {
     if ! type -p async_init > /dev/null; then
@@ -23,7 +97,7 @@ function ghq::dependences::check {
 function ghq::install {
     message_info "Installing ${ghq_package_name}"
     if type -p brew > /dev/null; then
-        brew install ${ghq_package_name}
+        brew install "${ghq_package_name}"
     fi
 
     ghq::post_install
