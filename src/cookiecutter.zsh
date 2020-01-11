81f9b2822f94879ef2b4ca2d49b56cb2d16ede20
@slovacus <slovacus@gmail.com>
2020-01-11 01:25:16 -0500

    (#29) chore: Implement function cookiecutter

---
+++ src/cookiecutter.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function cookiecutter::install {
+    message_info "Installing cookiecutter for ${ghq_package_name}"
+    if type -p pip > /dev/null; then
+        pip install --user cookiecutter
+        message_success "Installed cookiecutter for ${ghq_package_name}"
+    fi
+}
+
+function cookiecutter::list {
+    # shellcheck disable=SC2002
+    cat "${GHQ_SRC_DIR}"/data.json | jq '.projects[] | "\(.name) | \(.type) | \(.description) | \(.repository)"'
+}
+
+function cookiecutter::find {
+    local command_value
+    command_value=$(cookiecutter::list \
+                    | fzf \
+                    | awk '{print $(NF -0)}' \
+                    | perl -pe 'chomp' \
+                    | sed 's/\"//g'
+                )
+    cookiecutter "${command_value}"
+}
+
+if ! type -p cookiecutter > /dev/null; then cookiecutter::install; fi
