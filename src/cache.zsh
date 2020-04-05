6e478d04e4db7164e66699271f39918c0aac7374
@slovacus <slovacus@gmail.com>
2020-04-05 13:46:56 -0500

    refactor: function async (#67)

--- src/cache.zsh
+++ src/cache.zsh
@@ -18,7 +18,7 @@ function ghq::cache::create {
 function ghq::cache::create::async {
     async_init
     # Start a worker that will report job completion
-    async_start_worker ghq_worker_cache_make -n
+    async_start_worker ghq_worker_cache_make -u
     # Register our callback function to run when the job completes
     async_register_callback ghq_worker_cache_make ghq::completed::callback
     # Start the job
@@ -27,7 +27,7 @@ function ghq::cache::create::async {
 
 # Define a function to process the result of the job
 function ghq::completed::callback {
-    async_job ghq_worker_cache_make ghq::cache::create
+    message_success "task ghq::cache::create done!"
 }
 
 function ghq::cache::create::factory {
